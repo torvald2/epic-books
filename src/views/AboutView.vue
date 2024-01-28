50ce22f1e5eda04fbeefacd4db1a7011db241b27
torvald2 <valis.source@gmail.com>
2024-01-28 19:51:58 +0200

    fix

---
+++ src/views/AboutView.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+b-container 
+  b-row 
+    b-col
+      h1 Privacy Policy for Ruda Application
+      p
+        em Last Updated: 2024-01-28
+      p
+        | This Privacy Policy outlines how we handle information obtained from users in connection with the use of the &quot;E-Book Application&quot; (hereinafter referred to as the &quot;App&quot;). Please carefully read this document to understand what data we may collect and how we utilize the information we receive.
+      h2 1. Collected Information:
+      p
+        | We do not collect, store, or process any personal data about users through our App. We respect your privacy and aim to provide a secure and anonymous space for reading e-books.
+      h2 2. Use of Information:
+      p
+       | As we do not collect personal information, we do not use it for any purposes. Your private life remains your private life, and we do not seek to interfere with it.
+      h2 3. Security:
+      p
+        | We take measures to ensure the security of the information received from you, even if this information is not personal. However, please remember that no method of data transmission over the internet or electronic storage is 100% secure. We do our best to protect your data, but we cannot guarantee absolute security.
+      h2 4. Changes to the Privacy Policy:
+      p
+        | We reserve the right to make changes to our privacy policy from time to time. If we make any substantial changes, we will notify you by updating this page or through other reasonable means.
+      h2 5. Contact:
+      p
+        | If you have any questions or suggestions regarding our privacy policy, please contact us at: 
+        a(href='mailto:valis.source@gmail.com') valis.source@gmail.com
+      | .
+      p Thank you for using our E-Book Application!
+
+</template>
+<style scoped>
+ body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+}
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #666;
+        }
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+b-img(:src="require('../assets/ruda.png')" fluid-grow)
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
